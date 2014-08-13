6b8cd84163ea241f25cd23432ae1870b368d9a7e
therealeweezy <therealeweezy@gmail.com>
2014-08-13 16:09:09 +0000

    details to blind

---
+++ branches/blind.js
@@ -0,0 +1,39 @@
+
+var blindProcess=function(){
+	var s1=document.getElementById("blindTextInput").value;
+	var output="";
+	for(var i=0; i<s1.length; i++){
+		switch(s1.charAt(i)){
+		case '\n':
+			output+="<br/>";
+			break;
+		case 'r':
+			output+="<img src=\"http://www.puzzledragonx.com/en/img/icon1/0.png\" />";
+			break;
+		case 'u':
+			output+="<img src=\"http://www.puzzledragonx.com/en/img/icon1/1.png\" />";
+			break;
+		case 'g':
+			output+="<img src=\"http://www.puzzledragonx.com/en/img/icon1/2.png\" />";
+			break;
+		case 'w':
+			output+="<img src=\"http://www.puzzledragonx.com/en/img/icon1/3.png\" />";
+			break;
+		case 'p':
+			output+="<img src=\"http://www.puzzledragonx.com/en/img/icon1/4.png\" />";
+			break;
+		case 'h':
+			output+="<img src=\"http://www.puzzledragonx.com/en/img/icon1/5.png\" />";
+			break;
+		case 'x':
+			output+="<img src=\"http://www.puzzledragonx.com/en/img/icon1/6.png\" />";
+			break;
+		case '-':
+			output+="<img src=\"http://www.puzzledragonx.com/en/img/icon1/7.png\" />";
+			break;
+		default:
+			output+=s1.charAt(i);
+		}
+	}
+	document.getElementById("blindResult").innerHTML=output;
+};
